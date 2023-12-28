a6bad2ccd08ad5f9cc4f4de733d647bcae86b96b
bhawnai singh <root@DESKTOP-T23O176>
2023-12-28 18:26:07 +0530

    some changes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,7 +5,7 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.xpath("//input[@name='username']").type('Admin')
-    cy.xpath("//input[@name='password']").type('admin123')
+    // cy.xpath("//input[@name='password']").type('admin123')
     cy.get("button[type='submit']").click()
 })
 })
