dff2c295701311fa43dbfe6c5db6383c656ce41c
bhawani <shekhawat.bs13@gmail.com>
2024-01-10 14:15:15 +0530

    new websitetest

--- cypress/e2e/ecommerceWebsiteTest.cy.js
+++ cypress/e2e/ecommerceWebsiteTest.cy.js
@@ -1,9 +1,14 @@
 /// <reference types = 'Cypress' />
+const fs = require('fs').promises;
+
 
 describe('Ecommerce Website',()=>{
 
     it('passes',()=>{
         cy.visit("https://www.automationteststore.com/") 
-        cy.get()
+        cy.get('#customer_menu_top > li > a').click()
+        cy.xpath("//input[@id='loginFrm_loginname']").type('bhawani')
+        cy.xpath("//input[@id='loginFrm_password']").type('singh')
+        cy.xpath("//button[@title='Login']//i[@class='fa fa-lock']").click()
     })
 })
