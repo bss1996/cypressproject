878d6f1224a663b58d0c88c088ec41ff6e66229c
bhawani <shekhawat.bs13@gmail.com>
2024-01-29 13:22:13 +0530

    some add code

--- cypress/e2e/iterat-over-elements.cy.js
+++ cypress/e2e/iterat-over-elements.cy.js
@@ -3,8 +3,27 @@
 describe('Iterate over elements',()=>{
     it('Navigating to specific product pages',()=>{
         cy.visit("https://automationteststore.com/")
-        // cy.get('#categorymenu >nav>ul>li >a').contains("Hair Care").click()
-        cy.xpath('//*[@id="categorymenu"]/nav/ul/li[7]/a').click()
+        cy.get('#categorymenu >nav>ul>li >a').contains("Hair Care").click()
+        // cy.xpath('//*[@id="categorymenu"]/nav/ul/li[7]/a').click()
+        cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list)=>{
+            cy.log("Index:" + index + " : " + $el.text())
+        })
 
     })
+
+    it('Add specific product to basker',()=>{
+        cy.visit("https://automationteststore.com/")
+        cy.get('#categorymenu >nav>ul>li >a').contains("Hair Care").click()
+    
+        cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list)=>{
+            if($el.text().includes('Pantene Pro-V Conditioner, Classic Care')){
+                cy.wrap($el).click()
+            }
+
+
+        })
+    
+    
+    })
+
 })
